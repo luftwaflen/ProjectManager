9afb4d777549209ee98bac5f84461701b46ae340
luftwaflen <nikitapilkevich@gmail.com>
2023-05-21 02:40:15 +0300

    configure Identity.UserManager

--- ProjectManagerCore/Contracts/Repositories/IRepository.cs
+++ ProjectManagerCore/Contracts/Repositories/IRepository.cs
@@ -5,7 +5,7 @@
         public IEnumerable<T> GetAll();
         public Task<IEnumerable<T>> GetAllAsync();
         public T GetById(int id);
-        public Task<T> GetAllAsync(int id);
+        public Task<T> GetByIdAsync(int id);
         public void Add(T model);
         public Task AddAsync(T model);
         public void Update(T model);
